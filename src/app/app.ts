7f56d2ddd59e80effd55983c89759fc7294f7cdb
HamiltonEngInf <106934320+Hathdert@users.noreply.github.com>
2025-07-02 10:38:29 +0100

    First Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
+// src/app/app.ts
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';           
+import { HttpClientModule } from '@angular/common/http'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,              
+  imports: [RouterOutlet, FormsModule, HttpClientModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected title = 'skillbridge-frontend';
