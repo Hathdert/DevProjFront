58b8131d2c341b0447395afbb6c3352db5f0acae
Bruno Lopes Furuya <114805570+brnlf1990@users.noreply.github.com>
2025-07-09 14:45:03 +0100

    Created candidated-profile, offert-view, application components

---
+++ src/app/pages/offert-view/offert-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offert-view',
+  imports: [],
+  templateUrl: './offert-view.html',
+  styleUrl: './offert-view.scss'
+})
+export class OffertView {
+
+}
