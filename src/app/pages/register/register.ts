a48e172f19d6b9bef9e6fb8f7337878e01329eda
rui1479 <ruiribeiro2003.rr@gmail.com>
2025-07-09 14:52:37 +0100

    Company Profile

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -11,7 +11,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
