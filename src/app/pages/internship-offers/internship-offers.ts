d58d88211f3e2c3f6bc7f4db74467a0be9b5eef9
HamiltonEngInf <106934320+Hathdert@users.noreply.github.com>
2025-07-12 11:16:27 +0100

    Design Improvements

--- src/app/pages/internship-offers/internship-offers.ts
+++ src/app/pages/internship-offers/internship-offers.ts
@@ -3,17 +3,19 @@ import { InternshipOfferSimple } from '../../models/internship-offer.model';
 import { InternshipOfferService } from '../../services/internship-offer.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-internship-offers',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './internship-offers.html',
   styleUrl: './internship-offers.scss'
-  
 })
 export class InternshipOffers {
-offers: InternshipOfferSimple[] = [];
+  offers: InternshipOfferSimple[] = [];
+  filteredOffers: InternshipOfferSimple[] = [];
   showAll = false;
+  searchTerm: string = '';
 
   constructor(private offerService: InternshipOfferService) {}
 
@@ -21,10 +23,23 @@ offers: InternshipOfferSimple[] = [];
     this.offerService.getAllSimpleOffers().subscribe({
       next: (data) => {
         this.offers = data;
+        this.updateFilteredOffers();
       },
       error: (err) => {
         console.error('Erro ao buscar ofertas:', err);
       }
     });
+  }
+
+  updateFilteredOffers(): void {
+    const term = this.searchTerm?.toLowerCase() || '';
+    let filtered = this.offers.filter(o =>
+      !term || o.title.toLowerCase().includes(term)
+    );
+    this.filteredOffers = this.showAll ? filtered : filtered.slice(0, 6);
+  }
+
+  onSearchTermChange(): void {
+    this.updateFilteredOffers();
   }
 }
