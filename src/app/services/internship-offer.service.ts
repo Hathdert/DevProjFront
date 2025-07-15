f17152c2250f0f5390a5fcf591133895a183fe12
Ronald Costa <ronald_costa96@hotmail.com>
2025-07-15 11:35:12 +0100

    Auto stash before merge of "RRibeiro" and "origin/RRibeiro"

--- src/app/services/internship-offer.service.ts
+++ src/app/services/internship-offer.service.ts
@@ -69,16 +69,16 @@ getOfferByIdToken(id: number): Observable<InternshipOfferSimple> {
   );
 }
 
-getCompanyByOfferId(offerId: number): Observable<CompanyOffer> {
-  return this.http.get<CompanyOffer>(`http://localhost:8080/api/companies/by-offer/${offerId}`);
-}
+  getCompanyByOfferId(offerId: number): Observable<CompanyOffer> {
+    return this.http.get<CompanyOffer>(`http://localhost:8080/api/companies/by-offer/${offerId}`);
+  }
 
-changeOfferStatus(offerId: number, status: boolean): Observable<InternshipOfferSimple> {
-  return this.http.patch<InternshipOfferSimple>(
-    this.apiUrlChangeStatus.replace('{offerId}', offerId.toString()),
-    status
-  );
-}
+  changeOfferStatus(offerId: number, status: boolean): Observable<InternshipOfferSimple> {
+    return this.http.patch<InternshipOfferSimple>(
+      this.apiUrlChangeStatus.replace('{offerId}', offerId.toString()),
+      status
+    );
+  }
 
 createApplication(application: any): Observable<any> {
   return this.http.post('http://localhost:8080/api/applications/new', application);
